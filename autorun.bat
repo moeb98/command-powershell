f9cbc098e0b2081b9422bbbdbae192665ade1c34
moeb98 <55314997+moeb98@users.noreply.github.com>
2020-08-17 14:35:47 +0200

    removed clear screen from doskey

--- autorun.bat
+++ autorun.bat
@@ -1,5 +1,5 @@
 @echo off
-doskey ls=cls $T$T dir /p
+doskey ls=dir /p
 doskey version=for /f "tokens=3" %%a in ('reg query ^"HKLM^\Software^\Microsoft^\Windows NT^\CurrentVersion^" /v ^"ReleaseID^"') do @echo %%a
 prompt $E[32;1m$P$G$E[0m
 whoami /groups | find "S-1-16-12288" > nul 
